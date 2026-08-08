61fe0eea81566e063fcfd24d8fa6d85b842cbbe2
Thomas <totoriou09@gmail.com>
2026-08-08 14:33:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const products = [
     id: 1, 
     name: "Chemise à Carreaux", 
     category: "chemise", 
-    price: 19.99, 
+    price: 00.01, 
     sizes: ["S", "M", "L", "XL"],
     colors: ["Noir"],
     description: "Coupe ample à manches courtes, style vintage à carreaux brodés avec double poche.",
